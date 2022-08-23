324bb3ac651c1d54f90053dc7241bc3a7eae3642
IIBystrov <i.i.bystrov@yandex.ru>
2022-08-23 12:23:36 +0300

    Style .header__burger (style.css)

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@
 const burger = document.querySelector(".burger");
 const burgerLineFirst = document.querySelector(".burger").firstElementChild;
 const burgerLineLast = document.querySelector(".burger").lastElementChild;
+const burgerLineCenter = burgerLineFirst.nextElementSibling;
+
+console.log(typeof burgerLineCenter);
 
 burger.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	burgerLineFirst.classList.toggle("burger__line--first");
 	burgerLineLast.classList.toggle("burger__line--last");
+	burgerLineCenter.classList.toggle("burger__line--center");
 })
