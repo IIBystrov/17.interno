1e9f4d5050159d837159c07958491b8d1db4b2f8
IIBystrov <i.i.bystrov@yandex.ru>
2022-08-23 11:08:55 +0300

    Feature .burger (main.js)

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@
 
 const burger = document.querySelector(".burger");
 const burgerLineFirst = document.querySelector(".burger").firstElementChild;
+const burgerLineLast = document.querySelector(".burger").lastElementChild;
 
 burger.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	burgerLineFirst.classList.toggle("burger__line--first");
+	burgerLineLast.classList.toggle("burger__line--last");
 })
